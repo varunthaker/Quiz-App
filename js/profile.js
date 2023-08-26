21f62b033bdf52e2521b6358ee49c5747dfbc84e
varun thaker <vthakar3@gmail.com>
2023-08-26 14:48:28 +0200

    issues solved

--- js/profile.js
+++ js/profile.js
@@ -1,8 +1,6 @@
 console.clear();
 const toggleButton = document.querySelector('[data-js="toggle-switch"]');
 const mainContainer = document.querySelector("body");
-console.log(toggleButton);
-console.log(mainContainer);
 
 toggleButton.addEventListener("click", () => {
   mainContainer.classList.toggle("toggle-class");
